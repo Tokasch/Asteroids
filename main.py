920419b5db43b009076c0a4e9434bbf2d71f74ae
Tokasch <t-schulz@live.de>
2025-02-26 19:04:22 +0100

    Created player.py and circleshape.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 pygame.init()
+fps_clock = pygame.time.Clock()
+dt = 0
 from constants import *
+from player import Player
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -9,16 +12,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = fps_clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-
-
-
 
 if __name__ == "__main__":
     main()
